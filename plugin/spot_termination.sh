a23224b96e66e4dde74a91b3322c18d3eccf5967
Tom Booth <tombooth@gmail.com>
2018-12-11 16:35:35 +0000

    Better timeouts

--- plugin/spot_termination.sh
+++ plugin/spot_termination.sh
@@ -5,7 +5,7 @@ NONOK=1
 UNKNOWN=2
 
 function curl_status_code() {
-    curl --silent --output /dev/stderr --write-out "%{http_code}" "$1"
+    curl --max-time 3 --silent --output /dev/stderr --write-out "%{http_code}" "$1"
 }
 
 status_code="$(curl_status_code http://169.254.169.254/latest/meta-data/spot/instance-action)"
