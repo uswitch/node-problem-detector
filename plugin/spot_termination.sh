18862a0d7fdf566519250bc80e9ebb96cfa15f61
Tom Booth <tombooth@gmail.com>
2018-12-12 14:52:58 +0000

    Convert to it being sh rather than bash

--- plugin/spot_termination.sh
+++ plugin/spot_termination.sh
@@ -1,19 +1,15 @@
-#!/bin/bash
+#!/bin/sh
 
 OK=0
 NONOK=1
 UNKNOWN=2
 
-function curl_status_code() {
-    curl --max-time 3 --silent --output /dev/stderr --write-out "%{http_code}" "$1"
-}
+status_code=`curl --max-time 3 --silent --output /dev/stderr --write-out "%{http_code}" "http://169.254.169.254/latest/meta-data/spot/instance-action"`
 
-status_code="$(curl_status_code http://169.254.169.254/latest/meta-data/spot/instance-action)"
-
-if [[ "${status_code}" == "404" ]]
+if [ "${status_code}" -eq "404" ]
 then
     exit $OK
-elif [[ "${status_code}" == "200" ]]
+elif [ "${status_code}" -eq "200" ]
 then
     exit $NONOK
 else
