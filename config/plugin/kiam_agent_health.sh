7d8a7e939c155aad50dc20b40ce306c8e82a448c
Rhys Emmas <rhys.emmas@uswitch.com>
2019-08-30 10:41:31 +0100

    Update meta-data curl path

--- config/plugin/kiam_agent_health.sh
+++ config/plugin/kiam_agent_health.sh
@@ -4,7 +4,7 @@ OK=0
 NONOK=1
 UNKNOWN=2
 
-CHECK_AGENT="$(curl -s --show-error --connect-timeout 1 http://169.254.169.254/latest/meta-data/instance-id/ 2>&1)"
+CHECK_AGENT="$(curl -s --show-error --connect-timeout 1 http://169.254.169.254/latest/meta-data/instance-id/plz_give_me_id 2>&1)"
 
 case $CHECK_AGENT in
   request\ blocked*)
