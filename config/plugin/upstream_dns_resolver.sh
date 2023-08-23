7a4f47afd6b7bc96bb7fd65d44d70264dc4684d0
surajnarwade <suraj.narwade@rvu.co.uk>
2023-08-23 11:45:07 +0100

    use IP address

--- config/plugin/upstream_dns_resolver.sh
+++ config/plugin/upstream_dns_resolver.sh
@@ -10,7 +10,7 @@ readonly fqdn_a_record="$1"
 
 # We send a single attempt only, without retries and with 1s timeout.
 # We expect to receive an IPv4 address for a given A record.
-dig_cmd_out="$(dig -t A @${LOCAL_DNS_RESOLVER_IP} +tries=1 +retry=0 +time=1 +short "${fqdn_a_record}" 2>&1 | head -n1)"
+dig_cmd_out="$(dig -t A @172.20.0.11 +tries=1 +retry=0 +time=1 +short "${fqdn_a_record}" 2>&1 | head -n1)"
 dig_cmd_return_code="$?"
 dig_cmd_out_ipv4ish="$(echo "${dig_cmd_out}" | grep -E -o "^([0-9]{1,3}[\.]){3}[0-9]{1,3}$")"
 
