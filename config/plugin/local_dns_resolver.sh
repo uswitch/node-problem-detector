a25ccfccea197eff839000797443edf3424d3e33
Mark McGarry <mark.mcgarry@rvu.co.uk>
2026-04-21 10:15:26 +0100

    get node-local-dns pod ip

--- config/plugin/local_dns_resolver.sh
+++ config/plugin/local_dns_resolver.sh
@@ -4,7 +4,21 @@ OK=0
 NONOK=1
 UNKNOWN=2
 
-readonly local_dns_resolver_ip="$1"
+if [ -z "${NODE_NAME}" ]; then
+    exit $UNKNOWN
+fi
+
+# Get the node-local-dns pod IP running on this node directly,
+# bypassing 169.254.20.10 which requires Cilium's eBPF path
+local_dns_resolver_ip="$(curl -s \
+    -H "Authorization: Bearer $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" \
+    --cacert /var/run/secrets/kubernetes.io/serviceaccount/ca.crt \
+    "https://${KUBERNETES_SERVICE_HOST}:${KUBERNETES_SERVICE_PORT}/api/v1/namespaces/kube-system/pods?labelSelector=k8s-app%3Dnode-local-dns&fieldSelector=spec.nodeName%3D${NODE_NAME}" \
+    2>/dev/null | jq -r '.items[0].status.podIP')"
+
+if [ -z "${local_dns_resolver_ip}" ] || [ "${local_dns_resolver_ip}" = "null" ]; then
+    exit $UNKNOWN
+fi
 
 dig_cmd_out="$(dig -t TXT @"${local_dns_resolver_ip}" +tries=1 +retry=0 +time=33 +noqr +noall +comments kubernetes.default.svc. 2>&1)"
 dig_cmd_return_code="$?"
